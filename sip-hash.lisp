cad3d173c71481c9aab332aa17efbb3e7da5849c
Robert Brown <robert.brown@gmail.com>
2012-06-26 20:40:15 -0400

    Use consistent function and macro naming.  Use DEFINE-MODIFY-MACRO to define all the place modifying macros.  Hash functions now handle END set to NIL in a manner consistent with standard CL sequence functions.  Add documentation strings to the definitions of HASH-2-4 and HASH-4-8.

--- sip-hash.lisp
+++ sip-hash.lisp
@@ -6,14 +6,14 @@
 (in-package #:sip-hash)
 (declaim #.*optimize-fast-unsafe*)
 
-(defmacro mod-2^64 (x) `(logand ,x #xffffffffffffffff))
+(defmacro mod-2^64 (x) `(logand ,x #.(ldb (byte 64 0) -1)))
+(defmacro +-u64 (x y) `(mod-2^64 (+ ,x ,y)))
+(defmacro ash-u64 (x y) `(mod-2^64 (ash ,x ,y)))
 
-(defmacro u64+ (x y) `(mod-2^64 (+ ,x ,y)))
-(defmacro ash64 (x y) `(mod-2^64 (ash ,x ,y)))
-
-(defmacro incf64 (place x) `(setf ,place (u64+ ,place ,x)))
-(defmacro logiorf (place x) `(setf ,place (logior ,place ,x)))
-(defmacro logxorf (place x) `(setf ,place (logxor ,place ,x)))
+(define-modify-macro incf-u64 (x) +-u64 "Increment, modulo 2^64, PLACE by X.")
+(define-modify-macro logiorf (x) logior)
+(define-modify-macro logxorf (x) logxor)
+(define-modify-macro rotatef-u64 (x) rotate-u64 "Rotate 64-bit PLACE left by X bit positions.")
 
 (defmacro case-fall-through (key-form &body cases)
  "Equivalent to CASE, but control \"falls-through\" from one case form to the
@@ -33,15 +33,16 @@ next. Use (RETURN) to exit the CASE-FALL-THROUGH."
          ,@(reverse body)
          ,end))))
 
-(declaim (inline rotate-left-64))
+(declaim (inline rotate-u64))
 
-(defun rotate-left-64 (x shift)
- (declare (type uint64 x)
-          (type (integer 7 53) shift))
- #+sbcl
- (sb-rotate-byte:rotate-byte shift (byte 64 0) x)
- #-sbcl
- (logior (ash x shift) (mod-2^64 (ash x (- shift 64)))))
+(defun rotate-u64 (x count)
+  "Rotates the unsigned 64-bit integer X left by COUNT bit positions."
+  (declare (type uint64 x)
+           (type (integer 0 63) count))
+  #+sbcl
+  (sb-rotate-byte:rotate-byte count (byte 64 0) x)
+  #-sbcl
+  (logior (ash x count) (mod-2^64 (ash x (- count 64)))))
 
 (declaim (inline load-64))
 
@@ -57,41 +58,29 @@ next. Use (RETURN) to exit the CASE-FALL-THROUGH."
          (ash (aref octets (+ index 6)) 48)
          (ash (aref octets (+ index 7)) 56)))
 
-;; (defmacro sip-round (v0 v1 v2 v3)
-;;   `(macrolet ((left (x y rotation)
-;;                 `(progn (incf64 ,x ,y)
-;;                         (setf ,y (logxor (rotate-left-64 ,y ,rotation) ,x))
-;;                         (setf ,x (rotate-left-64 ,x 32))))
-;;               (right (x y rotation)
-;;                 `(progn (incf64 ,x ,y)
-;;                         (setf ,y (logxor (rotate-left-64 ,y ,rotation) ,x)))))
-;;      (progn (left ,v0 ,v1 13)
-;;             (right ,v2 ,v3 16)
-;;             (right ,v0 ,v3 21)
-;;             (left ,v2 ,v1 17))))
-
-(defmacro rotatef64 (place x) `(setf ,place (rotate-left-64 ,place ,x)))
-
 (defmacro sip-round (v0 v1 v2 v3)
-  `(progn (incf64 ,v0 ,v1)      (incf64 ,v2 ,v3)
-          (rotatef64 ,v1 13)    (rotatef64 ,v3 16)
-          (logxorf ,v1 ,v0)     (logxorf ,v3 ,v2)
-          (rotatef64 ,v0 32)
-          (incf64 ,v2 ,v1)      (incf64 ,v0 ,v3)
-          (rotatef64 ,v1 17)    (rotatef64 ,v3 21)
-          (logxorf ,v1 ,v2)     (logxorf ,v3 ,v0)
-          (rotatef64 ,v2 32)))
+  `(progn (incf-u64 ,v0 ,v1)        (incf-u64 ,v2 ,v3)
+          (rotatef-u64 ,v1 13)      (rotatef-u64 ,v3 16)
+          (logxorf ,v1 ,v0)         (logxorf ,v3 ,v2)
+          (rotatef-u64 ,v0 32)
+          (incf-u64 ,v2 ,v1)        (incf-u64 ,v0 ,v3)
+          (rotatef-u64 ,v1 17)      (rotatef-u64 ,v3 21)
+          (logxorf ,v1 ,v2)         (logxorf ,v3 ,v0)
+          (rotatef-u64 ,v2 32)))
 
-(defmacro define-sip-hash (function-name compress-rounds finalization-rounds)
-  `(defun ,function-name (octets k0 k1 &key (start 0) (end (length octets)))
+(defmacro define-sip-hash (function-name compress-rounds finalization-rounds documentation)
+  `(defun ,function-name (octets k0 k1 &key (start 0) end)
+     ,documentation
      (declare (type octet-vector octets)
               (type uint64 k0 k1)
-              (type vector-index start end))
+              (type vector-index start)
+              (type (or null vector-index) end))
      (let ((v0 (logxor k0 #x736f6d6570736575))
            (v1 (logxor k1 #x646f72616e646f6d))
            (v2 (logxor k0 #x6c7967656e657261))
            (v3 (logxor k1 #x7465646279746573))
            (index start))
+       (unless end (setf end (length octets)))
        ;; Compress each 64-bit message block.
        (loop while (<= index (- end 8)) do
          (let ((m (load-64 octets index)))
@@ -100,7 +89,7 @@ next. Use (RETURN) to exit the CASE-FALL-THROUGH."
            (logxorf v0 m)
            (incf index 8)))
        ;; Compress the last message block.
-       (let ((last-m (ash64 (- end start) 56)))
+       (let ((last-m (ash-u64 (- end start) 56)))
          (case-fall-through (- end index)
            (7 (logiorf last-m (ash (aref octets (+ index 6)) 48)))
            (6 (logiorf last-m (ash (aref octets (+ index 5)) 40)))
@@ -117,5 +106,9 @@ next. Use (RETURN) to exit the CASE-FALL-THROUGH."
        ,@(make-list finalization-rounds :initial-element '(sip-round v0 v1 v2 v3))
        (logxor v0 v1 v2 v3))))
 
-(define-sip-hash hash-2-4 2 4)
-(define-sip-hash hash-4-8 4 8)
+(define-sip-hash hash-2-4 2 4
+  "Returns SipHasp-2-4 hash code for positions START through END of OCTETS,
+using the initial state stored in K0 and K1.")
+(define-sip-hash hash-4-8 4 8
+  "Returns SipHasp-4-8 hash code for positions START through END of OCTETS,
+using the initial state stored in K0 and K1.")
