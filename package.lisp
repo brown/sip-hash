a56b4631e2373916be8d4e69f004d8c1727a695c
Robert Brown <robert.brown@gmail.com>
2013-05-07 20:32:13 -0400

    Change package description comment.

--- package.lisp
+++ package.lisp
@@ -4,7 +4,7 @@
 (in-package #:common-lisp-user)
 
 (defpackage #:sip-hash
- (:documentation "An implementation of the sip-hash hash function.")
+ (:documentation "An implementation of the SipHash family of hash functions.")
  (:use #:common-lisp #:com.google.base)
  (:export #:hash-2-4
           #:hash-4-8))
